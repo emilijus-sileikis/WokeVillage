4fd9b417c87aa284ea9591dec4f4bb4f6aa1506f
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-09-13 19:36:36 +0300

    Initial commit

---
+++ src/main/java/lt/vu/mif/it/paskui/paskui/Main.java
@@ -0,0 +1,16 @@
+package lt.vu.mif.it.paskui.paskui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
