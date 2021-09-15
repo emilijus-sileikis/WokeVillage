745f2f772aaaa8d693d86bd89938685109cddbb0
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-09-15 11:27:11 +0300

    Fixed JDK misconfigure and missing imports

--- src/main/java/lt/vu/mif/it/paskui/paskui/Main.java
+++ src/main/java/lt/vu/mif/it/paskui/paskui/Main.java
@@ -15,6 +15,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
