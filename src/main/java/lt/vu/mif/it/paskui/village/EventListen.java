9f0e3976e9fe21bf9b8e8bca64b37e7886b92a0b
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2021-10-18 13:09:15 +0300

    Event changes
    1) Inventory rework (Now its a dropper).
    2) Item glitch fix(You can not put items into the GUI anymore).
    3) Remove a certain amount of items function removeItems().

--- src/main/java/lt/vu/mif/it/paskui/village/EventListen.java
+++ src/main/java/lt/vu/mif/it/paskui/village/EventListen.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -21,8 +22,10 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.bukkit.craftbukkit.v1_17_R1.util.CraftMagicNumbers.getItem;
@@ -51,11 +54,12 @@ public class EventListen implements Listener {
     }
 
     public static void createInv(Inventory inv) {
-        Main.inv = Bukkit.createInventory(null, InventoryType.BARREL,
+        Main.inv = Bukkit.createInventory(null, InventoryType.HOPPER,
                 Component.text("Configuration Menu")
                         .decorate(TextDecoration.BOLD)
                         .color(NamedTextColor.AQUA)
         );
+
         org.bukkit.inventory.ItemStack item = new org.bukkit.inventory.ItemStack(Material.AMETHYST_SHARD);
         ItemMeta meta = item.getItemMeta();
         meta.displayName(
@@ -66,21 +70,7 @@ public class EventListen implements Listener {
         Lore.add( Component.text("Click to select").color(NamedTextColor.GRAY) );
         meta.lore(Lore);
         item.setItemMeta(meta);
-        Main.inv.setItem(0, item);
-
-        //Copy paste kiek reik
-
-        //close button
-        item.setType(Material.BARRIER);
-        meta.displayName(Component.text("Close")
-                .color(NamedTextColor.RED)
-                .decorate(TextDecoration.BOLD)
-                .decoration(TextDecoration.ITALIC, false)
-        );
-        Lore.clear();
-        meta.lore(Lore);
-        item.setItemMeta(meta);
-        Main.inv.setItem(8, item);
+        Main.inv.setItem(1, item);
 
         //WoodChopping Button
         item.setType(Material.STONE_AXE);
@@ -92,7 +82,27 @@ public class EventListen implements Listener {
         Lore.clear();
         meta.lore(Lore);
         item.setItemMeta(meta);
-        Main.inv.setItem(1, item);
+        Main.inv.setItem(2, item);
+
+        //close button
+        item.setType(Material.BARRIER);
+        meta.displayName(Component.text("Close")
+                .color(NamedTextColor.RED)
+                .decorate(TextDecoration.BOLD)
+                .decoration(TextDecoration.ITALIC, false)
+        );
+        Lore.clear();
+        meta.lore(Lore);
+        item.setItemMeta(meta);
+        Main.inv.setItem(3, item);
+
+        item.setType(Material.WHITE_STAINED_GLASS_PANE);
+        meta.displayName(Component.text(""));
+        Lore.clear();
+        meta.lore();
+        item.setItemMeta(meta);
+        Main.inv.setItem(4, item);
+        Main.inv.setItem(0, item);
     }
 
     @EventHandler
@@ -103,27 +113,47 @@ public class EventListen implements Listener {
         if (event.getCurrentItem().getItemMeta() == null) {return;}
         if (event.getCurrentItem().getItemMeta().displayName() == null) {return;}
 
-        event.setCancelled(true);
+        if (event.getWhoClicked() instanceof Player){
 
-        Player player = (Player) event.getWhoClicked();
+            Player p = (Player) event.getWhoClicked();
+            org.bukkit.inventory.@Nullable ItemStack is = event.getCurrentItem();
 
-        if (event.getSlot() == 0) {
+            if (is == null || is.getType() == Material.AIR || is.getType() == null) { event.setCancelled(true); }
 
-            player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Hello! Welcome to Paskui Plugin");
-            player.closeInventory();
-        }
-        if (event.getSlot() == 1) {
-            ItemStack item = new ItemStack(getItem(Material.GOLD_INGOT));
-            ItemMeta meta = item.asBukkitCopy().getItemMeta();
-            item.asBukkitCopy().setItemMeta(meta);         // <--- needs checking kurie iš šitų lines būtini
-            if (player.getInventory().contains(Material.GOLD_INGOT))
-            {
-                player.getInventory().remove(Material.GOLD_INGOT);
-                player.updateInventory();
-                player.sendMessage(ChatColor.GREEN + "You have bought lumberjack services!");
+            if (event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) { event.setCancelled(true); }
+
+            if (event.getClick().isKeyboardClick()) { event.setCancelled(true); }
+
+            if (event.getSlot() == 1) {
+
+                p.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Hello!");
+                p.closeInventory();
             }
-            player.closeInventory();
+
+            if (event.getSlot() == 2) {
+                ItemStack item = new ItemStack(getItem(Material.GOLD_INGOT));
+                ItemMeta meta = item.asBukkitCopy().getItemMeta();
+                item.asBukkitCopy().setItemMeta(meta);         // <--- needs checking kurie iš šitų lines būtini
+                if (p.getInventory().contains(Material.GOLD_INGOT))
+                {
+                    //p.getInventory().remove(Material.GOLD_INGOT);
+                    //p.getInventory().removeItem(item);
+                    removeItems(p.getInventory(), Material.GOLD_INGOT, 20);
+                    p.updateInventory();
+                    p.sendMessage(ChatColor.GREEN + "You have bought lumberjack services!");
+                }
+                p.closeInventory();
+            }
+
+            if (event.getSlot() == 3) {
+
+                p.closeInventory();
+            }
+
+            event.setCancelled(true);
         }
+
+        event.setCancelled(true);
     }
 
     @EventHandler
@@ -138,6 +168,28 @@ public class EventListen implements Listener {
             return;
 
         NPCManager.addJoinPacket(event.getPlayer());
+    }
+
+    @EventHandler
+    public static int removeItems(Inventory inventory, Material type, int amount) {
+
+        if(type == null || inventory == null)
+            return -1;
+        if (amount <= 0)
+            return -1;
+
+        if (amount == Integer.MAX_VALUE) {
+            inventory.remove(type);
+            return 0;
+        }
+
+        HashMap<Integer, org.bukkit.inventory.ItemStack> retVal = inventory.removeItem(new org.bukkit.inventory.ItemStack(type,amount));
+
+        int notRemoved = 0;
+        for(org.bukkit.inventory.ItemStack item: retVal.values()) {
+            notRemoved+=item.getAmount();
+        }
+        return notRemoved;
     }
 
     @EventHandler
