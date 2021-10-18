32ab9e234a4d75b9ecd138583b2aab6ae97aec7d
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-10-18 17:17:34 +0300

    Added ArgumentType::getArgClass()

--- src/main/java/lt/vu/mif/it/paskui/village/command/CommandFlag.java
+++ src/main/java/lt/vu/mif/it/paskui/village/command/CommandFlag.java
@@ -1,27 +1,32 @@
 package lt.vu.mif.it.paskui.village.command;
 
+import org.bukkit.Location;
+
 /**
  * Enum for storing necessary information about command flags
  */
 enum CommandFlag {
-    CMD_ARGUMENT("ARG", 0),
-    NPC_LOCATION("-l", 3),
-    NPC_NAME("--name", "-n", 1);
+    CMD_ARGUMENT("ARG", 0, String.class),
+    NPC_LOCATION("-l", 3, Location.class),
+    NPC_NAME("--name", "-n", 1, String.class);
 
     private final String flag;
     private final String alias;
     private final int argCount;
+    private final Class<?> clazz;
 
-    CommandFlag(String flag, int argCount) {
+    CommandFlag(String flag, int argCount, Class<?> clazz) {
         this.flag = flag;
         this.alias = flag;
         this.argCount = argCount;
+        this.clazz = clazz;
     }
 
-    CommandFlag(String flag, String alias, int argCount) {
+    CommandFlag(String flag, String alias, int argCount, Class<?> clazz) {
         this.flag = flag;
         this.alias = alias;
         this.argCount = argCount;
+        this.clazz = clazz;
     }
 
     public String getFlag() {
@@ -34,6 +39,10 @@ enum CommandFlag {
 
     public int getArgCount() {
         return argCount;
+    }
+
+    public Class<?> getArgClass() {
+        return clazz;
     }
 
     public static CommandFlag fromString(String val) {
