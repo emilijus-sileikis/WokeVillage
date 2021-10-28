9c4dbe84445353a563c743a77555e2bd28db5386
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-10-28 18:17:54 +0300

    Refactored DataManager.java

--- src/main/java/lt/vu/mif/it/paskui/village/DataManager.java
+++ src/main/java/lt/vu/mif/it/paskui/village/DataManager.java
@@ -11,7 +11,7 @@ import java.util.logging.Level;
 
 public class DataManager {
 
-    private Main plugin;
+    private final Main plugin;
     private FileConfiguration dataConfig = null;
     private File configFile = null;
 
@@ -22,46 +22,48 @@ public class DataManager {
     }
 
     public void reloadConfig() {
+        if (this.configFile == null) {
+            this.configFile = new File(this.plugin.getDataFolder(), "data.yml");
+        }
+
+        this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
+        InputStream defaultStream = this.plugin.getResource("data.yml");
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(
+                    new InputStreamReader(defaultStream)
+            );
+            this.dataConfig.setDefaults(defaultConfig);
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        if (this.dataConfig == null)
+            reloadConfig();
+
+        return  this.dataConfig;
+    }
+
+    public void saveConfig() {
+        if (this.dataConfig == null || this.configFile == null)
+            return;
+
+        try {
+            this.getConfig().save(this.configFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Can not save config to " + this.configFile, e);
+        }
+    }
+
+    public void saveDefaultConfig() {
         if (this.configFile == null)
             this.configFile = new File(this.plugin.getDataFolder(), "data.yml");
 
-            this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
-            InputStream defaultStream = this.plugin.getResource("data.yml");
-            if (defaultStream != null) {
-                YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
-                this.dataConfig.setDefaults(defaultConfig);
-
-            }
-        }
-
-        public FileConfiguration getConfig() {
-            if (this.dataConfig == null)
-                reloadConfig();
-
-            return  this.dataConfig;
-        }
-
-        public void saveConfig() {
-            if (this.dataConfig == null || this.configFile == null)
-                return;
-
-            try {
-                this.getConfig().save(this.configFile);
-            } catch (IOException e) {
-                plugin.getLogger().log(Level.SEVERE, "Can not save config to " + this.configFile, e);
-            }
-        }
-
-        public void saveDefaultConfig() {
-            if (this.configFile == null)
-                this.configFile = new File(this.plugin.getDataFolder(), "data.yml");
-
-            if (!this.configFile.exists()) {
-                this.plugin.saveResource("data.yml", false);
-            }
-        }
-
-        public void clearConfig() {
-            this.configFile.delete();
+        if (!this.configFile.exists()) {
+            this.plugin.saveResource("data.yml", false);
         }
     }
+
+    public void clearConfig() {
+        this.configFile.delete();
+    }
+}
