106a1d1568253425c8801ac7fe446f200a388d8a
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-11-21 00:25:16 +0200

    NPCEntity code clean up and javadoc

--- src/main/java/lt/vu/mif/it/paskui/village/npc/entities/NPCEntity.java
+++ src/main/java/lt/vu/mif/it/paskui/village/npc/entities/NPCEntity.java
@@ -1,39 +1,86 @@
 package lt.vu.mif.it.paskui.village.npc.entities;
 
 import lt.vu.mif.it.paskui.village.npc.NPC;
-import lt.vu.mif.it.paskui.village.npc.Role;
 import net.minecraft.world.entity.Entity;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
+/**
+ * Abstraction layer for minecraft entities
+ * representing NPC in world.
+ */
 public interface NPCEntity {
     // getters
+    /**
+     * Gets name given to minecraft entity.
+     * @return minecraft entity name in {@link String}.
+     */
     String getEntityName();
 
+    /**
+     * Getter for entity uuid.
+     * @return entity {@link UUID}.
+     */
     UUID getEntityUUID();
 
+    /**
+     * Getter for bukkit entity.
+     * @return abstracted minecraft entity in {@link org.bukkit.entity.Entity}.
+     */
     org.bukkit.entity.Entity getBukkitEntity();
 
+    /**
+     * Getter for minecraft entity.
+     * @return raw minecraft entity.
+     */
     Entity getNMSEntity();
 
-    /** Returns attached NPC to Entity
+    /**
+     * Returns attached NPC to Entity.
      * @return NPC instance.
      */
     NPC getNPC();
 
     // setters
-    void setEntityName(String name);
+    /**
+     * Setter for minecraft entity name.
+     * @param name name that entity will be given.
+     */
+    void setEntityName(final @NotNull String name);
 
-    void setEntityPos(Location loc);
+    /**
+     * Setter for updating minecraft entity locations.
+     * @param loc location minecraft entity will be placed at.
+     */
+    void setEntityPos(final @NotNull Location loc);
 
-    void setEntityUUID(UUID npcUUID);
+    /**
+     * Setter for unique id on minecraft entity.
+     * Should only be called only when instantiating
+     * already created npc.
+     * @param npcUUID unique id of minecraft entity to be set.
+     */
+    void setEntityUUID(final UUID npcUUID);
 
     // public
+    /**
+     * Ends trading with player.
+     */
     void stopEntityTrading();
 
+    /**
+     * Used to remove NPC entity from world.
+     */
     void removeEntity();
 
+    /**
+     * Makes the NPC go to the nearest Spruce Log block.
+     * Then waits some time to simulate chopping.
+     * @param timeElapsed time for collecting resources.
+     * @param material    material to find and collect.
+     */
     void moveTo(int timeElapsed, Material material);
 }
