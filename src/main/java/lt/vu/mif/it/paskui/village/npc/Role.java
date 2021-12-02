9e5e4d4c5923160e51274bbdc88f8a0fb5458f71
Robertas Timukas <robertas.timukas@mif.stud.vu.lt>
2021-12-02 20:53:02 +0200

    Fixed role mismatch in NPC name

--- src/main/java/lt/vu/mif/it/paskui/village/npc/Role.java
+++ src/main/java/lt/vu/mif/it/paskui/village/npc/Role.java
@@ -14,7 +14,7 @@ public enum Role {
 
     private final Class<? extends SelectionScreen> clazz;
 
-    Role(Class<? extends SelectionScreen> clazz) {
+    Role(final Class<? extends SelectionScreen> clazz) {
         this.clazz = clazz;
     }
 
@@ -33,7 +33,8 @@ public enum Role {
 
     @Override
     public String toString() {
-        return super.toString().toLowerCase();
+        return super.toString().substring(0, 1)
+            + super.toString().substring(1).toLowerCase();
     }
 
     public static Role getRandomRole() {
