5feb72c3ee70fce2cfa3952d0ffeaa0193b53c6d
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2022-01-12 16:01:18 +0200

    NPC cure mechanic
    Cured zombievillagers have 25% chance to turn into NPCs and turned nitwit zombievillagers (not naturally spawned ones, but turned by a zombie) have a 100% chance to become an NPC after curing them.

--- src/main/java/lt/vu/mif/it/paskui/village/EventListen.java
+++ src/main/java/lt/vu/mif/it/paskui/village/EventListen.java
@@ -1,28 +1,14 @@
 package lt.vu.mif.it.paskui.village;
 
-import io.papermc.paper.event.packet.PlayerChunkLoadEvent;
 import lt.vu.mif.it.paskui.village.commands.NPCNames;
+import lt.vu.mif.it.paskui.village.npc.NPC;
 import lt.vu.mif.it.paskui.village.npc.NPCManager;
 import lt.vu.mif.it.paskui.village.npc.Personality;
 import lt.vu.mif.it.paskui.village.npc.Role;
 import lt.vu.mif.it.paskui.village.npc.events.NPCDeathEvent;
 import lt.vu.mif.it.paskui.village.npc.services.SelectionScreen;
-import net.kyori.adventure.text.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity.RemovalReason;
-import net.minecraft.world.entity.monster.ZombieVillager;
-import net.minecraft.world.entity.npc.VillagerProfession;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.StructureType;
-import org.bukkit.World;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.craftbukkit.v1_17_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftVillagerZombie;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityCategory;
-import org.bukkit.entity.EntityType;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftVillager;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
@@ -32,15 +18,12 @@ import org.bukkit.event.entity.EntityTransformEvent;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
-import org.jetbrains.annotations.NotNull;
 
 public class EventListen implements Listener {
 
     private final NPCManager npcManager;
     private final DataManager dataManager;
-    boolean newChunk;
-    int limit = 0;
+    boolean nitwit = false;
 
     public EventListen(NPCManager npcManager, DataManager dataManager) {
         this.npcManager = npcManager;
@@ -87,21 +70,32 @@ public class EventListen implements Listener {
         }
     }
 
-/*    @EventHandler
-    public void turnEvent (EntityTransformEvent event) {
-        if (event.getTransformedEntity().getType().equals(EntityType.ZOMBIE_VILLAGER)) {
-            Bukkit.broadcast(Component.text("A Villager was turned!"));
+    @EventHandler
+    public boolean turnEvent (EntityTransformEvent event) {
+        if (event.getEntity() instanceof CraftVillager) {
+            CraftVillager villager = (CraftVillager) event.getEntity();
+            if (villager.getProfession().equals(Villager.Profession.NITWIT)) {
+               return nitwit = true;
+            }
+            else return nitwit = false;
         }
+        return false;
     }
-    
- */
 
     @EventHandler
     public void spawnEvent (CreatureSpawnEvent event) {
-        if (event.getEntity() instanceof Villager && event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CURED) {
+        int rng = SelectionScreen.randomInt(1, 4);
+        if (event.getEntity() instanceof Villager && event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CURED && nitwit) {
             event.getEntity().remove();
             String name = NPCNames.getRandomName().getName() + " The " + Role.getRandomRole().toStringWithCapInitial();
-            npcManager.createNPC(name, event.getLocation(), Role.getRandomRole(), Personality.getRandomPersonality());
+            NPC npc = npcManager.createNPC(name, event.getLocation(), Role.getRandomRole(), Personality.getRandomPersonality());
+            if (npc != null) { dataManager.writeData(npc, npc.id); }
+        }
+        else if (event.getEntity() instanceof Villager && event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CURED && rng == 1) {
+            event.getEntity().remove();
+            String name = NPCNames.getRandomName().getName() + " The " + Role.getRandomRole().toStringWithCapInitial();
+           NPC npc = npcManager.createNPC(name, event.getLocation(), Role.getRandomRole(), Personality.getRandomPersonality());
+            if (npc != null) { dataManager.writeData(npc, npc.id); }
         }
     }
 }
